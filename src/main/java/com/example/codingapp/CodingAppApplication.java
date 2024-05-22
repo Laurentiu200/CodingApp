38774006f2418f5cfe7344da9c6663741374a37e
Laurentiu200 <laurentiu.borza@student.upt.ro>
2024-05-22 13:21:11 +0300

    Initial commit

---
+++ src/main/java/com/example/codingapp/CodingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codingapp/CodingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
